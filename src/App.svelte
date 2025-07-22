9dbd35e192dab1336fa1547daabd84a2143a99a4
idlevi29 <vanhao294.ts@gmail.com>
2025-07-22 22:08:54 +0700

    uh thi

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from './routes/Header.svelte';
+	import Footer from './routes/Footer.svelte';
+	import Intro from './routes/Intro.svelte';
+	import { writable } from 'svelte/store';
+	let showIntro = writable(true);
+</script>
+
+<Header />
+{#if $showIntro}
+	<Intro {showIntro} />
+{:else}
+	<slot />
+{/if}
+<Footer />
--- src/routes/music.js
+++
@@ -1,6 +0,0 @@
-// Example structure for music.js
-// Each entry: { name: string, url: string }
-export default [
-  { name: "Metao", url: "https://www.youtube.com/watch?v=xRL2BspFnOs" },
-  // Add more tracks here as needed
-];
